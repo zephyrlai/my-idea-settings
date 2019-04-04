ca86f62ade8ed18af6c0920971e4ca7bae6d03a6
zephyrlai <laizonghao@foxmail.com>
2019-04-04 17:36:03 +0800

    IC-2018.3.5 <zephyr@DESKTOP-MV54554 Create baseRefactoring.xml, ide.general.xml, project.default.xml, Enum.java, Interface.java, lombok-plugin.xml, other.xml, Default.xml, find.xml, ui.lnf.xml, filetypes.xml, projectView.xml, javaeeExternalResources.xml, colors.scheme.xml, mavenVersion.xml, AnnotationType.java, laf.xml, Class.java, jdk.table.xml, path.macros.xml, vcs.xml, editor.codeinsight.xml, editor.xml, debugger.xml, updates.xml

---
+++ fileTemplates/internal/Interface.java
@@ -0,0 +1,9 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+/**
+ * @Author: zephyrLai
+ * @Description: TODO
+ * @Date: ${DATE} ${TIME}
+ */
+public interface ${NAME} {
+}
